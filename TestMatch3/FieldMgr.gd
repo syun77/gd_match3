5317d3076bf1959ea720d7d76e8979a8ed831db7
syun <2dgames.jp@gmail.com>
2022-11-09 16:51:48 +0900

    消去チェックを定数化

--- TestMatch3/FieldMgr.gd
+++ TestMatch3/FieldMgr.gd
@@ -19,6 +19,15 @@ const HEIGHT = 8 # フィールドの高さ.
 const OFS_X = 32 # フィールドの描画オフセット(X).
 const OFS_Y = 32 # フィールドの描画オフセット(Y).
 
+const ERASE_CNT = 3 # 3つ並んだら消去する.
+
+# 消去種別 (消去判定で使用する).
+enum eEraseType {
+	EMPTY  = 0 # 空.
+	NOT    = 1 # 検索済み(消さない).
+	REMOVE = 2 # 消去対象.
+}
+
 # ----------------------------------------
 # メンバ変数.
 # ----------------------------------------
@@ -34,7 +43,10 @@ onready var _layer = $Layer # タイル管理用キャンバスレイヤー.
 # ----------------------------------------
 # 初期化.
 func initialize() -> void:
+	# フィールド情報を初期化.
 	_field.fill(Array2.EMPTY)
+	
+	# タイルをすべて消しておく.
 	remove_all()
 
 # 生成したタイルをすべて破棄する.
@@ -44,6 +56,12 @@ func remove_all() -> void:
 
 # 更新.
 func _process(_delta: float) -> void:	
+	
+	# 消去判定を行う.
+	_update_erase()
+
+# 更新 > 消去処理
+func _update_erase() -> void:
 	# いったん初期化.
 	_field.fill(Array2.EMPTY)
 	
@@ -64,16 +82,18 @@ func _process(_delta: float) -> void:
 		var x = _field.to_x(idx)
 		var y = _field.to_y(idx)
 		var n = _field.getv(x, y)
-		print("erase[%d]: (x, y) = (%d, %d)"%[n, x, y])
+		#print("erase[%d]: (x, y) = (%d, %d)"%[n, x, y])
 		_field.set_idx(idx, Array2.EMPTY)
 
 		# 消滅開始.		
 		var tile = search_tile(x, y)
-		tile.start_vanish()
-		
+		tile.start_vanish()	
+
+# タイル情報を取得する.
 func getv(i:int, j:int) -> int:
 	return _field.getv(i, j)
-	
+
+# 下のタイルとの衝突チェックする.
 func check_hit_bottom(tile:TileObj) -> bool:
 	if tile.get_now_y() >= HEIGHT - 1:
 		return true
@@ -133,8 +153,6 @@ func fall() -> void:
 func check_erase() -> PoolIntArray:
 	var erase_list = PoolIntArray()
 	
-	# 1: 検索済み.
-	# 2: 消去する.
 	var tmp = Array2.new(WIDTH, HEIGHT)
 	
 	for j in range(_field.height):
@@ -145,8 +163,9 @@ func check_erase() -> PoolIntArray:
 				continue
 			
 			for k in range(2):
-				tmp.fill(0)
-				tmp.setv(i, j, 2) # 消せるかもしれない候補.
+				# 初期化.
+				tmp.fill(eEraseType.EMPTY)
+				tmp.setv(i, j, eEraseType.REMOVE) # 消せるかもしれない候補.
 				var cnt = 1
 				
 				# 上下を調べる.
@@ -156,11 +175,11 @@ func check_erase() -> PoolIntArray:
 					tbl = [[-1, 0], [1, 0]]
 				
 				for v in tbl:
-					cnt = _check_erase_around(tmp, n, cnt, i, j, v[0], v[1])
+					cnt = _check_erase_recursive(tmp, n, cnt, i, j, v[0], v[1])
 				
-				if cnt >= 3:
-					# 3つ以上連続していれば消せる.
-					var list = tmp.search(2)
+				if cnt >= ERASE_CNT:
+					# ERASE_CNT以上連続していれば消せる.
+					var list = tmp.search(eEraseType.REMOVE)
 					erase_list.append_array(list)
 	
 	# 重複インデックスを削除
@@ -172,23 +191,23 @@ func check_erase() -> PoolIntArray:
 	return list
 
 # 消去チェック (再帰処理用).
-func _check_erase_around(tmp:Array2, n:int, cnt:int, x:int, y:int, vx:int, vy:int) -> int:
+func _check_erase_recursive(tmp:Array2, n:int, cnt:int, x:int, y:int, vx:int, vy:int) -> int:
 	# 移動先を調べる.
 	var x2 = x + vx
 	var y2 = y + vy
-	if tmp.getv(x2, y2) == 1:
+	if tmp.getv(x2, y2) == eEraseType.NOT:
 		# 探索済み.
 		return cnt
 	
 	var n2 = _field.getv(x2, y2)
 	if n != n2:
 		# 不一致なので消せない.
-		tmp.setv(x2, y2, 1)
+		tmp.setv(x2, y2, eEraseType.NOT)
 		return cnt
 	
 	# 消せるかもしれない.
 	cnt += 1
-	tmp.setv(x2, y2, 2)
+	tmp.setv(x2, y2, eEraseType.REMOVE)
 	# 次を調べる.
-	return _check_erase_around(tmp, n, cnt, x2, y2, vx, vy)
+	return _check_erase_recursive(tmp, n, cnt, x2, y2, vx, vy)
 
